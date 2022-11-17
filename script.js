8b0dfe898676b0d5e1e3e0df0a4a2896b02e28fa
YuJaeHyuk <fd730@naver.com>
2022-11-17 10:09:57 +0900

    navbar 기능추가

--- script.js
+++ script.js
@@ -3,9 +3,10 @@
 // 스크롤에 따른 메뉴바 색상처리
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
-const home = document.querySelector('.home__title');
-const home2 = document.querySelector('.home__description');
-const homeHeight = navbar.getBoundingClientRect().height;
+
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+
 console.log(navbarHeight);
 document.addEventListener('scroll', () => {
     // console.log('이벤트가 발생되었음!');
@@ -20,13 +21,14 @@ document.addEventListener('scroll', () => {
     }else{
         uparrow.classList.remove('up--arrow');
     }
-    home.style.opacity = 1 - window.scrollY / homeHeight;
-    home2.style.opacity = 1 - window.scrollY / homeHeight;
+    home.style.opacity = 1-window.scrollY / homeHeight;
 });
 
+
+
 // 스크롤에 따른 메뉴바 고정
-const navbarMeunu = document.querySelector('.navbar__menu');
-navbarMeunu.addEventListener('click', (e) => {
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (e) => {
     // console.log(e);
     const target = e.target;
     const link = target.dataset.link;
@@ -34,8 +36,17 @@ navbarMeunu.addEventListener('click', (e) => {
         return;
     }
     // console.log(link);
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
 });
+
+
+
+// 모바일 메뉴 버튼 설정
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
+})
 
 const hp = document.querySelector('.home__contact');
 hp.addEventListener('click', (e) => {
@@ -47,7 +58,102 @@ uparrow.addEventListener('click', (e) => {
     scrollIntoView('#home');
 });
 
-function scrollIntoView(selecotor){
-    const scrollTo = document.querySelector(selecotor);
+function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({ behavior: 'smooth'});
+    selectNavItem(navItems[sectionIds.indexOf(selector)]);
 };
+
+
+
+
+// 선택한 프로젝트 보이기
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects  = document.querySelectorAll('.project');
+
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if(filter == null){
+        return;
+    }
+    const active = document.querySelector('.category__btn.selected');
+    if(active != null){
+        active.classList.remove('selected');
+    }
+    e.target.classList.add('selected');
+
+    projectContainer.classList.add('anim-out');
+    setTimeout(() => {
+        projects.forEach((project) => {
+            console.log(project.dataset.type);
+            if(filter === '*' || filter === project.dataset.type){
+                project.classList.remove('invisible');
+            }else{
+                project.classList.add('invisible');
+            }
+        });
+        projectContainer.classList.remove('anim-out');
+    }, 300);
+})
+
+
+const sectionIds = [
+    '#home',
+    '#about',
+    '#skills',
+    '#work',
+    '#testimonials',
+    '#contact'
+];
+// const sections에 map으로 하나씩 찾아서 id에 넣어서 document.querySelector(id)를 sections에 배열형식으로 저장
+const sections = sectionIds.map((id) => document.querySelector(id));
+// console.log(sections);
+const navItems = sectionIds.map((id) => document.querySelector(`[data-link="${id}"]
+`));
+// console.log(navItems);
+
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+
+function selectNavItem(selected){
+    selectedNavItem.classList.remove('active');
+    selectedNavItem = selected;
+    selectedNavItem.classList.add('active');
+}
+
+const observerOptions = {
+    root:null,
+    rootMargin: '0px',
+    threshold: 0.3
+}
+
+const observerCallback = (entries, observer) =>{
+    entries.forEach((entry) => {
+        if(!entry.isIntersecting && entry.intersectionRatio > 0){
+            console.log('y');
+            const index = sectionIds.indexOf(`#${entry.target.id}`);
+            // console.log(index);
+            if(entry.boundingClientRect.y < 0){
+                selectedNavIndex = index + 1;
+            }else{
+                selectedNavIndex = index - 1;
+            }
+            console.log(selectedNavItem);
+        }
+    })
+}
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach((section) => observer.observe(section));
+
+window.addEventListener('wheel', () => {
+    if(window.scrollY === 0){
+        selectedNavIndex = 0;
+    }else if(
+        window.scrollY + window.innerHeight === document.body.clientHeight
+    ){
+        selectedNavIndex = navItems.length - 1;
+    }
+    selectNavItem(navItems[selectedNavIndex]);
+})
